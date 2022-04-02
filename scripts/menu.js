ec93835549627a7bdddd4b362a41286750d5f656
Yasir Khan <Yasirkahn6497@gmail.com>
2022-04-02 02:00:23 +0100

    hero section completed

--- scripts/menu.js
+++ scripts/menu.js
@@ -40,7 +40,9 @@ function menuBackground(){
         //-- if logo is up so place it up while scrollbar is at top
         if(window.innerWidth >= 1200)
         menu.style.paddingTop = "4rem";
+        else menu.style.paddingTop = "0rem";
         }
+        
 
         menu.style.background = menu_background;
         main_logo.style.color= logo_color;
